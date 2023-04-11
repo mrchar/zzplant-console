8e92936463a9c01f5e9a065bb5fa0f34e15d8fc1
mrchar <mrchar@qq.com>
2023-04-11 21:55:29 +0800

    安装ElementPlus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+const count = ref(1)
 </script>
 
 <template>
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
+    <el-button @click="count++">{{ count }}</el-button>
 </template>
